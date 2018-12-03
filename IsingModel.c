8f3333606a18141ee06959b43125f4b78bc86643
MoursalXD <45019750+MoursalXD@users.noreply.github.com>
2018-12-03 16:32:00 +0100

    Add files via upload
    Ajout de Method_Approach

--- IsingModel.c
+++ IsingModel.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define size 4
+int size;
 
 // function to intialize all spins to -1
 void imInitAllDown(int matSpin[size][size])
@@ -60,9 +60,58 @@ void displayMatSpin(int matSpin[size][size])
 }
 
 
+void Method_Approach(int matSpin[size][size])
+{
+	int i,j,c1=0,	c2=0, c3=0 , c4=0, SpinBord=0;
+	int cP, mP=0;
+
+/*===============Calcul constante m' : les spin vers le bas dans la grill============*/
+
+for ( i = 0; i< size; ++i)
+	for(j=0; j< size; ++j)
+		if(matSpin[i][j] == -1)
+			mP++;
+
+
+/*==============Calcul de la constante C' : les spin vers bas sur les côtés dans la grill=================*/
+
+	for (i = 0; i < size; ++i)
+	{
+		if(matSpin[i][0] == -1)
+			c1++;		// spin vers le bas sur la gauche
+
+		if(matSpin[i][size -1] == -1)
+			c3++;  // nbr de spin vers le bas sur la droite
+		j+(size-1);
+	}	
+
+	
+	for (j = 0; j < size; ++j)
+		{
+			if(matSpin[0][j]== -1)
+				c2++;  //nbr de spin vers le bas sur la droite
+
+			if(matSpin[size -1][j] == -1)
+			c4++;  // nbr de spin vers le bas sur le bas						
+		}
+
+// Récupérer le nombre de spin sur les bords
+	for(i=0;i<size;i++)
+		for(j=0;j<size;j++)
+			if(((i==0 && j==0) || (i==0 && j==size-1) || (i== size-1 && j==0) || (i==size-1 && j==size-1)) && matSpin[i][j] == -1)
+					SpinBord++;			
+
+	cP = c1+c2+c3+c4-SpinBord;			
+	printf("c\' vaut %d\n", cP);
+	printf("m\' vaut %d\n", mP);
+	printf("\n");
+
+/*===================================================*/
+}
+
 int main(int argc, char const *argv[]){
 
-
+	size = atoi(argv[1]);
 	int Mat[size][size];
 	
 	//imInitAllDown(matLattice, Rows);
@@ -70,6 +119,7 @@ int main(int argc, char const *argv[]){
 	//imInitAllUp(matLattice, Rows);
 	
 	imInitWarm(Mat);
+	Method_Approach(Mat);
 	displayMatSpin(Mat);
 	
 }
