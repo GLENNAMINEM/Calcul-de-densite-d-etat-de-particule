77bffe6c8ddb0f463442d2e95d63c0946f23d2ef
MoursalXD <45019750+MoursalXD@users.noreply.github.com>
2018-12-26 19:18:20 +0100

    Add files via upload
    La matrice est de nl*nc maintenant, l'exécution se fais :  ./IsingModel.c nl nc

--- IsingModel.c
+++ IsingModel.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int size;
+int nc,nl;
 
 // function to intialize all spins to -1
-void imInitAllDown(int matSpin[size][size])
+void imInitAllDown(int matSpin[nl][nc])
 {
-	for (int i=0; i<size; i++)
+	for (int i=0; i<nl; i++)
 	{
-		for (int j=0; j<size; j++)
+		for (int j=0; j<nc; j++)
 		{
 			matSpin[i][j] = -1;
 		}
@@ -16,11 +16,11 @@ void imInitAllDown(int matSpin[size][size])
 }
 
 // function to intialize all spins to +1
-void imInitAllUp(int matSpin[size][size])
+void imInitAllUp(int matSpin[nl][nc])
 {
-	for (int i=0; i<size; i++)
+	for (int i=0; i<nl; i++)
 	{
-		for (int j=0; j<size; j++)
+		for (int j=0; j<nc; j++)
 		{
 			matSpin[i][j] = 1;
 		}
@@ -28,16 +28,16 @@ void imInitAllUp(int matSpin[size][size])
 }
 
 // function to intialize spins randomly
-void imInitWarm(int matSpin[size][size])
+void imInitWarm(int matSpin[nl][nc])
 {
 	double dRan;
 
-	for (int i=0; i<size; i++)
+	for (int i=0; i<nl; i++)
 	{
-		for (int j=0; j<size; j++)
+		for (int j=0; j<nc; j++)
 		{
 			dRan = rand()%2;
-			if (dRan <= 0.5)
+			if (dRan <= 0.6)
 			{
 			matSpin[i][j] = 1;
 			}
@@ -47,11 +47,11 @@ void imInitWarm(int matSpin[size][size])
 }
 
 //function to display spins
-void displayMatSpin(int matSpin[size][size])
+void displayMatSpin(int matSpin[nl][nc])
 {
-	for (int i = 0; i< size; ++i)
+	for (int i = 0; i< nl; ++i)
 	{
-		for (int  j= 0; j< size; ++j)
+		for (int  j= 0; j< nc; ++j)
 		{
 			printf("%d ", matSpin[i][j]);
 		}
@@ -60,39 +60,37 @@ void displayMatSpin(int matSpin[size][size])
 }
 
 
-void Method_Approach(int matSpin[size][size])
+void Method_Approach(int matSpin[nl][nc])
 {
 	int i,j,c1=0,	c2=0, c3=0 , c4=0, SpinBord=0;
-
 	int cP=0, mP=0;
-
 
 /*===============Calcul constante m' : les spin vers le bas dans la grill============*/
 
-for ( i = 0; i< size; ++i)
-	for(j=0; j< size; ++j)
+for ( i = 0; i< nl; ++i)
+	for(j=0; j< nc; ++j)
 		if(matSpin[i][j] == -1)
 			mP++;
 
 
 /*==============Calcul de la constante C' : les spin vers bas sur les côtés dans la grill=================*/
 
-	for (i = 1; i < size-1; ++i)
+	for (i = 1; i < nl-1; ++i)
 	{
 		if(matSpin[i][0] == -1)
 			c1++;		// spin vers le bas sur la gauche
 
-		if(matSpin[i][size -1] == -1)
+		if(matSpin[i][nc-1] == -1)
 			c3++;  // nbr de spin vers le bas sur la haute
 	}	
 
 	
-	for (j = 0; j < size; ++j)
+	for (j = 0; j < nc; ++j)
 		{
 			if(matSpin[0][j]== -1)
 				c2++;  // nbr de spin vers le bas sur la droite
 
-			if(matSpin[size -1][j] == -1)
+			if(matSpin[nl -1][j] == -1)
 			c4++;  // nbr de spin vers le bas sur le bas						
 		}
 
@@ -100,22 +98,20 @@ for ( i = 0; i< size; ++i)
 /*==================Calcul de la constante S' : le nombre de paires d'atomes adjacents ayant différentes spins========*/
 		int s = 0;
 
-		for (i = 0; i < size-1; ++i)
-			for(j = 0; j < size-2; ++j)
+		for (i = 0; i < nl-1; ++i)
+			for(j = 0; j < nc-2; ++j)
 				if(matSpin[i][j] != matSpin[i][j+1])
 					s+=1;
 					
 				
-		for (i = 0; i < size-1; ++i)
-			for(j = 0; j < size-2; ++j)
+		for (i = 0; i < nl-1; ++i)
+			for(j = 0; j < nc-2; ++j)
 				if(matSpin[i][j] != matSpin[i+1][j])
 					s+=1;
 
+
 	cP = c1+c2+c3+c4;			
-	printf("c1 %d\n", c1);
-	printf("c3 %d\n", c3);
-	printf("c2 %d\n", c2);
-	printf("c4 %d\n", c4);
+
 	printf("c\' vaut %d\n", cP);
 	printf("m\' vaut %d\n", mP);
 	printf("s\' vaut %d\n", s);
@@ -126,8 +122,9 @@ for ( i = 0; i< size; ++i)
 
 int main(int argc, char const *argv[]){
 
-	size = atoi(argv[1]);
-	int Mat[size][size];
+	nl = atoi(argv[1]);
+	nc = atoi(argv[2]);
+	int Mat[nl][nc];
 	
 	//imInitAllDown(matLattice, Rows);
 	
