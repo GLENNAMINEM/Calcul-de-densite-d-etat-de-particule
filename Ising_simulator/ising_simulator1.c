64a04be3d9bb235ddf0d50c1aa51012e715df436
MoursalXD <45019750+MoursalXD@users.noreply.github.com>
2019-04-11 14:26:08 +0200

    Add files via upload
    Mise à jours avec la constante de Bolztmann et la Température mais sa reste a faire un tableau de température :
    c-à-d variation de la température pour savoir une telle température sa change en quoi.

--- Ising_simulator/ising_simulator1.c
+++ Ising_simulator/ising_simulator1.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <math.h>
+#include <time.h>
 #include "list.h"
-#define N 6     // grid size
-#define runs 10   // number of Monte Carlo simulations
+#define N 10     // grid size
+#define runs 10000  // number of Monte Carlo simulations
 
 
 // returns up (1) or down (-1)
 int updown()
 {
+  
   double randomNumber;
-  randomNumber = rand()%2;
+  randomNumber = rand()%2;//drand48();
   if (randomNumber < 0.5) return (-1);
   else return 1;
 }
@@ -22,15 +25,15 @@ double energy(int spin[N][N])
   int i,j;
   double E=0.;
 
-  for (i=0; i<N; i++)
-    for (j=0; j<N; j++)
-    {
-      if (i-1>=0) E -= 0.5*(spin[i][j]*spin[i-1][j]-1.);
-      if (j-1>=0) E -= 0.5*(spin[i][j]*spin[i][j-1]-1.);
-      if (i+1<N)  E -= 0.5*(spin[i][j]*spin[i+1][j]-1.);
-      if (j+1<N)  E -= 0.5*(spin[i][j]*spin[i][j+1]-1.);
-    }
-
+  for (i=0; i<N; i++){
+	    for (j=0; j<N; j++)
+	    {
+	      if (i-1>=0) E -= 0.5*(spin[i][j]*spin[i-1][j]-1.);
+	      if (j-1>=0) E -= 0.5*(spin[i][j]*spin[i][j-1]-1.);
+	      if (i+1<N)  E -= 0.5*(spin[i][j]*spin[i+1][j]-1.);
+	      if (j+1<N)  E -= 0.5*(spin[i][j]*spin[i][j+1]-1.);
+	    }
+	}
   return E;    
 }
 
@@ -105,6 +108,26 @@ for ( i = 0; i< N; ++i)
 /*===================================================*/
 }
 
+// Il s'agit de la constante de Bolztmann 1,380 648 52 × 10**(−23)
+float k_bolztmann(){
+  const signed long EXPONENT = -23;
+  
+  float value = 0;
+  signed long n = 0;
+  
+  if (EXPONENT < 0)
+  {
+    value = 10;
+    
+    for (n = -1; n > EXPONENT; n--)
+    {
+      value *= 10;
+    }
+    
+    value = 1.0 / value;
+  }
+  return value*1.38064852;
+}
 
 void monte_carlo(int spin[N][N],ListNode *list_node,int run){
 
@@ -113,44 +136,62 @@ void monte_carlo(int spin[N][N],ListNode *list_node,int run){
     time_t t;
     node d;
 
-      //srand(time(NULL));
+    //srand(time(NULL));
       //srand48(5123);
       //srand((unsigned) time(&t));
-    ai=rand()%(N-1);
-    aj=rand()%(N-1);
+    
+    ai=rand()%(N);
+    aj=rand()%(N);
 
+    //printf("ai = %d et aj = %d à l'itération %d \n", ai, aj, run);
     spin[ai][aj];
  
     en_old = energy(spin);  // energy before flip
 
     // flip
     spin[ai][aj] = -spin[ai][aj];
+    
+
+
+
 
     en_new = energy(spin);  // energy after flip
 
+    //double beta = ;// J/KT avec J = 1 et K : constant de Boltzmann et T temperature 
+
     H = en_new - en_old;    // difference in energy
 
-    if (H>0)
+    
+    if (H<=0)
     {
-       r = (rand()%N)/(N-1);  //drand48();
-       if (r>exp(-H)){
+       //r = drand48();
+       //if (r>exp(-H)){
         //printf("ai=%d aj=%d faux\n",ai,aj);
-        spin[ai][aj] = -spin[ai][aj];  // Undo flip
-
-        }else {
+        
+    	//printf("ai = %d et aj = %d à l'itération %d \n", ai, aj, run);
+        //}else {
         //printf("ai=%d aj=%d :vrai 1\n",ai,aj);
           d = Method_Approach(spin);
-          insertion(list_node, d.m,d.s,d.c);
+          //insertion(list_node, d.m,d.s,d.c);
           searchListCmdElment(list_node, d.m,d.s,d.c);
-        }
+        //}
     }    
     else {
-    //printf("ai=%d aj=%d :vrai 2\n",ai,aj);
-         d = Method_Approach(spin);
-              //insertion(list_node, d.m,d.s,d.c);
-              searchListCmdElment(list_node, d.m,d.s,d.c);
+
+      r = drand48();
+      float T = 240; // La Température 
+      float kt = k_bolztmann() * T;
+      if (r < exp(-H/kt))
+      {
+        spin[ai][aj] = -spin[ai][aj];  // Undo flip
+        d = Method_Approach(spin);
+        //insertion(list_node, d.m,d.s,d.c);
+        searchListCmdElment(list_node, d.m,d.s,d.c);
+      }
+
      }
-}
+  
+} 
 
 
 int main(int argc , char** argv)
@@ -165,13 +206,16 @@ int main(int argc , char** argv)
   //FILE *data;
 
   // initial fill
+  srand( time(NULL) );
   for (i=0; i<N; i++)
   {
     for (j=0; j<N; j++)
     {
       spin[i][j] = updown();
+      printf("[%d] ", spin[i][j]);
       //spinAccum[0][i][j] = spin[i][j];
     }
+    printf("\n");
   }
   
   for(run = 0; run<runs; run++)
@@ -180,7 +224,8 @@ int main(int argc , char** argv)
     //printf("Completed run #%i\n",run);
 
   
-} 
+  } 
+
  afficherListCmd(list_node);
  free(list_node);
 //fclose(data);
