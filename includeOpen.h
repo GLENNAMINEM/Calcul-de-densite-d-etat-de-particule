e98f604a07153935e59a35655082dde7f5f491f4
MoursalXD <45019750+MoursalXD@users.noreply.github.com>
2019-05-02 09:10:52 +0200

    Update includeOpen.h

--- includeOpen.h
+++ includeOpen.h
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,62 +8,74 @@
 //#define runs 100 // number of Monte Carlo simulations
 
  int cnt=0;
-int ok = 0;
+
 
 //add new configuration in array
 
 void add_bloc(int * tab,int a,int b, int c){
 
-	int faux=0;
-	int p,test=0;
 
+ int faux=0;
+ int p,test=0;
+
+  //#pragma omp parallel private(cnt, tab,a,b,c)
+  //{
     	if(cnt==0){
     		test++;
     	}
 
 
+    //#pragma omp parallel for reduction(+:cnt) //reduction(+:faux)
      for(p=0;p<cnt;p=p+4){
 
     	   if(tab[(p)]==a && tab[(1)+(p)]==b && tab[(2)+(p)]==c){
-    	      tab[(3)+(p)]+= 1;
+
+           // #pragma omp atomic  
+    	       tab[(3)+(p)]+= 1;
     	   }
-    		else{ 
-         			faux+=4;
-      				if(faux == cnt) test++;
-        		}
+
+    	else  { 
+
+        //#pragma omp atomic
+          faux+=4;
+      		if(faux == cnt) test++;
+        }
 
       }
     
+
        if(test){
 
     		  tab[cnt]=a;tab[(1)+(cnt)]=b;tab[(2)+(cnt)]=c;
     		  tab[(3)+(cnt)]=1;
     			cnt+=4;
        	}
+    //}
 
   }
 
   
 
-//output as file array
+//display array
 
 void ising_output(int* tab, int cnttt){
 
-	int j;
-	long long Z = 0;
+int j;
 
-	FILE *data;
+FILE *data;
 
-	data = fopen("ising-output_Par4.dat","w");
+data = fopen("ising-output.dat","w");
 
-	for(j=0;j<cnttt;j=j+4){
+for(j=0;j<cnttt;j=j+4){
 
-		fprintf(data,"%i\t%i\t%i\t%i\n",tab[j],tab[1+j],tab[2+j],tab[3+j]);
-		printf("%llu\n",Z+=tab[3+j]);
-	}
+  //printf("m=%d c=%d s=%d z=%d\n",tab[j],tab[1+j],tab[2+j],tab[3+j]);
+
+  fprintf(data,"%i\t%i\t%i\t%i\n",tab[j],tab[1+j],tab[2+j],tab[3+j]);
+}
 
   fclose(data);
-}
+
+ }
 
 
 // returns up (1) or down (-1)
@@ -74,8 +87,9 @@ int updown()
   else return -1;
 }
 
+
 // Calculate energy
-double ith_energy(int spin[N][N])
+double energy(int spin[N][N])
 {
   int i,j;
   double E=0.;
@@ -93,40 +107,44 @@ double ith_energy(int spin[N][N])
 }
 
 
-
+//Méthode (type node) qui nous permet de retourner un noeud de "z", ex: z[m][s][c]
  int* Method_Approach(int matSpin[N][N])
 {
 
   int *t=malloc(sizeof(int)*3);
-  int i,j; 
+  int i,j; //c1=0, c2=0, c3=0 , c4=0;
   int cP=0, mP=0;
 
-/*===============Calcul constante m : la somme des spins de la grill============*/
+/*===============Calcul constante m' : les spin vers le bas dans la grill============*/
 
 for ( i = 0; i< N; ++i)
   for(j=0; j< N; ++j)
     mP+=matSpin[i][j];
 
 
-/*==============Calcul de la constante C : la somme des spins vers bas sur les côtés dans la grill=================*/
+/*==============Calcul de la constante C' : les spin vers bas sur les côtés dans la grill=================*/
 
   for (i = 1; i < N-1; ++i)
   {
-    //if(matSpin[i-1][0] == -1)
-      cP+=matSpin[i-1][0];   // spin vers le bas sur la gauche
+    if(matSpin[i][0] == -1)
+      cP+=matSpin[i][0];   // spin vers le bas sur la gauche
 
-    //if(matSpin[i][N-1] == -1)
+    if(matSpin[i][N-1] == -1)
       cP+=matSpin[i][N-1];  // nbr de spin vers le bas sur la haute
-
-    //if(matSpin[0][i]== -1)
-      cP+=matSpin[0][i];  // nbr de spin vers le bas sur la droite
-
-    //if(matSpin[N -1][i-1] == -1)
-      cP+=matSpin[N -1][i-1];  // nbr de spin vers le bas sur le bas
   }
 
 
-/*==================Calcul de la constante S : la somme des spins paires d'atomes adjacents ayant différentes sens========*/
+  for (j = 0; j < N; ++j)
+    {
+      if(matSpin[0][j]== -1)
+        cP+=matSpin[0][j];  // nbr de spin vers le bas sur la droite
+
+      if(matSpin[N -1][j] == -1)
+        cP+=matSpin[N -1][j];  // nbr de spin vers le bas sur le bas
+    }
+
+
+/*==================Calcul de la constante S' : le nombre de paires d'atomes adjacents ayant différentes spins========*/
     int s = 0;
 
     for (i = 0; i < N; ++i){
@@ -135,12 +153,16 @@ for ( i = 0; i< N; ++i)
         if(matSpin[i][j] != matSpin[i][j+1] && j != N-1  ){  //voisin à droite
           s+=matSpin[i][j];
         }
-		if(matSpin[i][j] != matSpin[i+1][j] && i != N-1){ //voisin en bas
+	if(matSpin[i][j] != matSpin[i+1][j] && i != N-1){ //voisin en bas
           s+=matSpin[i][j];
-		}
+	}
       }
     }
 
+
+
+
+  //cP = c1+c2+c3+c4;
 
 
   t[0]=mP;t[1]=cP;t[2]=s;
@@ -153,46 +175,43 @@ for ( i = 0; i< N; ++i)
 
 void monte_carlo(int spin[N][N],int* tab,int run, double beta){
 
-
-  	int ai,aj;
-
-  	int cnt2 = 0;
-  	double en_old,en_new,deltaE,r,Etotal = 0.;
-  
-  	int *ta=malloc(sizeof(int)*3);
+  int ai,aj;
+  double en_old,en_new,H,r;
+  int *ta=malloc(sizeof(int)*3);
 
 
-	srand48(5123);
+	srand48(omp_get_thread_num());
 
+  #pragma omp critical
+	{   
       ai=rand()%(N);
       aj=rand()%(N);
-  
+  }
 
         spin[ai][aj];
 
-       en_old = ith_energy(spin);  // energy before flip
+       en_old = energy(spin);  // energy before flip
 
 
       spin[ai][aj] = -spin[ai][aj];// flip
 
-      en_new = ith_energy(spin);  // energy after flip
-      
-      Etotal += en_new;
+      en_new = energy(spin);  // energy after flip
 
-      deltaE = en_new - en_old;    // difference in energy
+      H = en_new - en_old;    // difference in energy
 
      //#pragma omp critical 
      //{  
-        if (deltaE>0)
+        if (H>0)
         {
-           r = drand48();
-           if (r < exp(-deltaE*beta)){
+           r = (rand()%N)/(N-1);
+           if (r>exp(-H*beta)){
   		        spin[ai][aj] = -spin[ai][aj];  // Undo flip
 
            }else {
-           		
+
   		        ta=Method_Approach(spin);
-             	
+             
+
   		        add_bloc(tab,ta[0],ta[1],ta[2]);
             } 
 
@@ -200,7 +219,7 @@ void monte_carlo(int spin[N][N],int* tab,int run, double beta){
         else {
       		
               ta=Method_Approach(spin);
-             
+            
               add_bloc(tab,ta[0],ta[1],ta[2]);
             }  
             
