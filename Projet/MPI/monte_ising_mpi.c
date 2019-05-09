b0b6ab209726125da267ca3cb4bc0989d28c9041
GLENNAMINEM <32958307+GLENNAMINEM@users.noreply.github.com>
2019-05-09 23:14:55 +0200

    Add files via upload

---
+++ Projet/MPI/monte_ising_mpi.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+#include "include_mpi.h"
+
+
+
+int main(int argc, char**argv)
+{
+
+  //int N = atoi(argv[2]);
+  int i,j,spin[N][N],runs,size,rank,tag1=0;
+  double en_old,en_new,H,r;
+
+  
+    MPI_Status sta;
+    MPI_Request req;
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    runs = atoi(argv[1]);
+    time_t t = time(NULL);
+    int run,z=0,cnt=0;
+    int *buf=NULL;
+    int* tab = NULL;
+    //double t1,t2;
+
+
+
+     if(rank == 0){
+
+        tab = (int*)malloc((sizeof(int)*runs*4+1)*size);
+        buf= (int*)malloc(sizeof(int)*runs*4*(size-1));
+  
+               }
+     else tab = (int*)malloc(sizeof(int)*runs*4+1);
+
+
+
+
+
+
+  // initial fill
+  for (i=0; i<N; i++)
+  {
+    for (j=0; j<N; j++)
+    { 	
+     srand(t+t*rank); //ne fonction du temps et du rang
+      spin[i][j] = updown(rank);
+
+    }
+  }
+
+ 
+
+  //srand(t*rank); //ne fonction du temps et du rang
+
+  
+  for(int run = 0; run<runs; run++)
+  { 
+  	 int Z = 0;
+
+    for( int e = 4 ; e < cnt; e += 4)
+   { 
+
+    Z += tab[e] ; 
+   } 
+   printf("Z=%d\n",Z);
+
+    if ( Z ==  pow(2 , N*N +N) ) break ;
+ 
+    else monte_carlo(spin,tab,run,rank,&cnt);
+
+
+
+   }
+
+
+   //communication to send array
+       //t1 = MPI_Wtime();
+   if(rank != 0){
+
+        MPI_Send(tab, cnt, MPI_INT, 0, tag1, MPI_COMM_WORLD);
+
+                 }
+
+   if(rank == 0 && size>1){
+ 
+       for(int i=1;i<size;i++){
+
+        MPI_Recv(buf + runs*4*(i-1), runs*4, MPI_INT,i, tag1, MPI_COMM_WORLD, &sta);
+             }
+
+	//t2=MPI_Wtime();
+       for(int j=1;j<size;j++){
+
+        fusion(tab, buf + runs*4*(j-1),&cnt, &buf[runs*4*(j-1)]); 
+
+                     }
+
+
+        ising_output(tab,cnt);
+        display(tab,&cnt);
+  	//printf("MPI_Wtime %f\n", t2-t1);
+
+   }
+   display(tab,&cnt);
+   //ising_output(tab,cnt);
+
+ //MPI_Barrier(MPI_COMM_WORLD);
+
+ free_array(tab,buf,rank);
+
+
+   MPI_Finalize();
+
+  return 0;
+}
